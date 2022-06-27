6d9bb4b3e08f157bac670b99bd77a263e1f2b2d0
toktar <renata.toktar@dsr-corporation.com>
2022-06-27 19:46:09 +0300

    Add did url utils

---
+++ utils/did_url.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"regexp"
+)
+
+var ResourcePath, _ = regexp.Compile(`resource\\\[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`)
+
+func GetResourceId(didUrlPath string) (id string) {
+	if !ResourcePath.Match([]byte(didUrlPath)) {
+		return ""
+	}
+
+	match := ResourcePath.FindStringSubmatch(didUrlPath)
+	return match[1]
+}
