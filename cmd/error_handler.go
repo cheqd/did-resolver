98f3a7c0214e255728c9821637de25cde24e7a8b
toktar <renata.toktar@dsr-corporation.com>
2022-09-01 17:09:30 +0300

    Use new errors

---
+++ cmd/error_handler.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/cheqd/did-resolver/types"
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
+)
+
+func CustomHTTPErrorHandler(err error, c echo.Context) {
+
+	if err == nil {
+		return
+	}
+	identityError := generateIdentityError(err)
+	if identityError.Code == 500 {
+		log.Error().Err(identityError.Internal)
+	} else {
+		log.Warn().Err(identityError.Internal)
+	}
+	c.Response().Header().Set(echo.HeaderContentType, string(identityError.ContentType))
+	c.JSONPretty(identityError.Code, identityError.DispalayMessage(), "  ")
+
+}
+
+func generateIdentityError(err error) *types.IdentityError {
+	identityError, ok := err.(*types.IdentityError)
+	if ok {
+		return identityError
+	}
+	he, ok := err.(*echo.HTTPError)
+	if !ok || he.Code != 404 {
+		return types.NewInternalError("", types.JSON, err, false)
+	}
+	return types.NewInvalidDIDUrlError("", types.JSON, err, false)
+}
