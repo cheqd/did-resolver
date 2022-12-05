eb02f0053ef2fa02bf0d4223ca2f36053a772703
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-12-05 15:43:21 +0000

    Update base details

--- main.go
+++ main.go
@@ -27,12 +27,13 @@ func printVersion(cmd *cobra.Command, args []string) error {
 // @title DID Resolver for did:cheqd method
 // @version 1.x
 // @description Universal Resolver driver for did:cheqd method
-
 // @contact.name Cheqd Foundation Limited
 // @contact.url https://cheqd.io
-
 // @license.name Apache 2.0
 // @license.url https://github.com/cheqd/did-resolver/blob/main/LICENSE
+// host https://resolver.cheqd.net
+// basePath /1.0/identifiers
+// schemes http https
 func main() {
 	rootCmd := cmd.GetRootCmd()
 	rootCmd.AddCommand(getVersionCmd())
