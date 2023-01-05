b72b534e91f2f87a74abe61e8333cfdfeeb3f84a
DaevMithran <daevmithran1999@gmail.com>
2023-01-05 19:13:04 +0530

    fix: Fix importkeys script

--- tests/docker/cheqd-node/scripts/generate.sh
+++ tests/docker/cheqd-node/scripts/generate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# list keyring
+cheqd-noded keys list --keyring-backend test
+
 # publish dids
-cheqd-noded tx cheqd create-did did.json --from base_account_1 --keyring-backend test --gas auto --gas-adjustment 1.2 --gas-prices 50ncheq --chain-id cheqd
+cheqd-noded tx cheqd create-did did.json --from base_account_1 --keyring-backend test --fees 5000000ncheq --chain-id cheqd
 
 # publish resources
-cheqd-noded tx resource create resource.json --from base_account_1 --keyring-backend test --gas auto --gas-adjustment 1.2 --gas-prices 25ncheq --chain-id cheqd
+# cheqd-noded tx resource create resource.json --from base_account_1 --keyring-backend test --fees 5000000ncheq --chain-id cheqd
