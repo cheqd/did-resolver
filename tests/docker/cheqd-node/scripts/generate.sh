a419dac06f445e0ac347ab09b5b511fa1b73e1e5
DaevMithran <daevmithran1999@gmail.com>
2023-01-19 12:47:43 +0530

    build: Add delay before script execution

--- tests/docker/cheqd-node/scripts/generate.sh
+++ tests/docker/cheqd-node/scripts/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # publish dids
-cheqd-noded tx cheqd create-did did.json --from base_account_1 --keyring-backend test --fees 5000000ncheq --chain-id cheqd
+cheqd-noded tx cheqd create-did did.json --from base_account_1 --keyring-backend test --fees 5000000ncheq --chain-id cheqd -y
 
 # publish resources
-# cheqd-noded tx resource create resource.json --from base_account_1 --keyring-backend test --fees 5000000ncheq --chain-id cheqd
+cheqd-noded tx resource create resource.json --from base_account_1 --keyring-backend test --fees 5000000ncheq --chain-id cheqd -y
