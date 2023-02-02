a9b3dea8497ca97947341d98f9941450b87a405e
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-02-02 18:46:05 +0500

    Remove unused functions and variables

--- utils/did_url.go
+++ utils/did_url.go
@@ -1,8 +1,6 @@
 package utils
 
 import (
-	"errors"
-	"regexp"
 	"strings"
 
 	migrations "github.com/cheqd/cheqd-node/app/migrations/helpers"
@@ -10,32 +8,8 @@ import (
 	"github.com/google/uuid"
 )
 
-var ResourceId, _ = regexp.Compile(`[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`)
-
-func IsDidUrl(didUrl string) bool {
-	_, path, query, fragmentId, err := didUtils.TrySplitDIDUrl(didUrl)
-	return err == nil && (path != "" || query != "" || fragmentId != "")
-}
-
 func IsValidResourceId(u string) bool {
 	_, err := uuid.Parse(u)
-	return err == nil
-}
-
-func ValidateV1ID(id string) error {
-	isValidId := len(id) == 16 && didUtils.IsValidBase58(id) ||
-		len(id) == 32 && didUtils.IsValidBase58(id) ||
-		didUtils.IsValidUUID(id)
-
-	if !isValidId {
-		return errors.New("unique id should be one of: 16 symbols base58 string, 32 symbols base58 string, or UUID")
-	}
-
-	return nil
-}
-
-func IsValidV1ID(id string) bool {
-	err := ValidateV1ID(id)
 	return err == nil
 }
 
@@ -44,10 +18,13 @@ func IsValidIndyV1ID(id string) bool {
 		len(id) == 32 && didUtils.IsValidBase58(id)
 }
 
+func IsValidUUIDV1ID(id string) bool {
+	return didUtils.IsValidUUID(id) && strings.ToLower(id) != id
+}
+
 func IsMigrationNeeded(id string) bool {
-	return len(id) == 16 && didUtils.IsValidBase58(id) ||
-		len(id) == 32 && didUtils.IsValidBase58(id) ||
-		didUtils.IsValidUUID(id) && strings.ToLower(id) != id
+	return IsValidIndyV1ID(id) || IsValidUUIDV1ID(id)
+
 }
 
 func MigrateDID(did string) string {
