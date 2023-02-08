34ed9c9bdb0e68394c2e16eb77e74fa7adc81623
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-02-08 13:05:00 +0500

    Time fields should be Timestamppb type

--- types/did_doc_metadata.go
+++ types/did_doc_metadata.go
@@ -6,24 +6,17 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-// Changed "Created time.Time" to "Create *time.Time".
-// It needs to skip Created field when is empty.
 type ResolutionDidDocMetadata struct {
-	Created     *time.Time             `json:"created,omitempty" example:"2021-09-01T12:00:00Z"`
-	Updated     *time.Time             `json:"updated,omitempty" example:"2021-09-10T12:00:00Z"`
+	Created     *timestamppb.Timestamp `json:"created,omitempty" example:"2021-09-01T12:00:00Z"`
+	Updated     *timestamppb.Timestamp `json:"updated,omitempty" example:"2021-09-10T12:00:00Z"`
 	Deactivated bool                   `json:"deactivated,omitempty" example:"false"`
 	VersionId   string                 `json:"versionId,omitempty" example:"4979BAF49599FEF0BAD5ED0849FDD708156761EBBC8EBE78D0907F8BECC9CB2E"`
 	Resources   []DereferencedResource `json:"linkedResourceMetadata,omitempty"`
 }
 
 func NewResolutionDidDocMetadata(did string, metadata did.Metadata, resources []*resource.Metadata) ResolutionDidDocMetadata {
-	created := &metadata.Created
-	if created.IsZero() {
-		created = nil
-	}
-
 	newMetadata := ResolutionDidDocMetadata{
-		Created:     created,
+		Created:     metadata.Created,
 		Updated:     metadata.Updated,
 		Deactivated: metadata.Deactivated,
 		VersionId:   metadata.VersionId,
