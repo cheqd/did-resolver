8d15ce41320d54720a035a66c5bc2eb246bc7058
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2023-02-22 19:19:41 +0000

    Update main.go

--- main.go
+++ main.go
@@ -7,16 +7,14 @@ import (
 //	@title			DID Resolver for cheqd DID method
 //	@version		v3.0
 //	@description	Universal Resolver driver for cheqd DID method
-
 //	@contact.name	Cheqd Foundation Limited
 //	@contact.url	https://cheqd.io
-
 //	@license.name	Apache 2.0
 //	@license.url	https://github.com/cheqd/did-resolver/blob/main/LICENSE
+//	@host			resolver.cheqd.net
+//	@BasePath		/1.0/identifiers
+//	@schemes		https http
 
-//	@host		resolver.cheqd.net
-//	@BasePath	/1.0/identifiers
-//	@schemes	https http
 func main() {
 	rootCmd := cmd.GetRootCmd()
 
