cd62032a200ab46ab53adcdc339c412233fbc17d
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-03-16 19:38:45 +0500

    Move swagger openAPI comments to one place for diddoc and resource handlers.

--- services/diddoc/echo_handlers.go
+++ services/diddoc/echo_handlers.go
@@ -9,6 +9,24 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// DidDocEchoHandler godoc
+//
+//	@Summary		Resolve DID Document on did:cheqd
+//	@Description	Fetch DID Document ("DIDDoc") from cheqd network
+//	@Tags			DID Resolution
+//	@Accept			application/did+ld+json,application/ld+json,application/did+json
+//	@Produce		application/did+ld+json,application/ld+json,application/did+json
+//	@Param			did			path		string	true	"Full DID with unique identifier"
+//	@Param			service		query		string	false	"Service Type"
+//	@Param			fragmentId	query		string	false	"#Fragment"
+//	@Param			versionId	query		string	false	"Version"
+//	@Success		200			{object}	types.DidResolution
+//	@Failure		400			{object}	types.IdentityError
+//	@Failure		404			{object}	types.IdentityError
+//	@Failure		406			{object}	types.IdentityError
+//	@Failure		500			{object}	types.IdentityError
+//	@Failure		501			{object}	types.IdentityError
+//	@Router			/{did} [get]
 func DidDocEchoHandler(c echo.Context) error {
 	// ToDo: Make fragment detection better
 	isFragment := len(strings.Split(c.Param("did"), "#")) > 1
@@ -28,11 +46,27 @@ func DidDocEchoHandler(c echo.Context) error {
 	}
 }
 
+// DidDocVersionEchoHandler godoc
+//
+//	@Summary		Resolve DID Document Version on did:cheqd
+//	@Description	Fetch specific all version of a DID Document ("DIDDoc") for a given DID and version ID
+//	@Tags			DID Resolution
+//	@Accept			application/did+ld+json,application/ld+json,application/did+json
+//	@Produce		application/did+ld+json,application/ld+json,application/did+json
+//	@Param			did			path		string	true	"Full DID with unique identifier"
+//	@Param			versionId	path		string	true	"version of a DID document"
+//	@Success		200			{object}	types.DidResolution
+//	@Failure		400			{object}	types.IdentityError
+//	@Failure		404			{object}	types.IdentityError
+//	@Failure		406			{object}	types.IdentityError
+//	@Failure		500			{object}	types.IdentityError
+//	@Failure		501			{object}	types.IdentityError
+//	@Router			/{did}/version/{versionId} [get]
 func DidDocVersionEchoHandler(c echo.Context) error {
 	return services.EchoWrapHandler(&DIDDocVersionRequestService{})(c)
 }
 
-// ResolveDIDDocVersionMetadata godoc
+// DidDocVersionMetadataEchoHandler godoc
 //
 //	@Summary		Resolve DID Document Version Metadata on did:cheqd
 //	@Description	Fetch metadata of specific a DID Document ("DIDDoc") version for a given DID and version ID
@@ -51,21 +85,21 @@ func DidDocVersionMetadataEchoHandler(c echo.Context) error {
 	return services.EchoWrapHandler(&DIDDocVersionMetadataRequestService{})(c)
 }
 
-// ResolveAllDidDocVersionsMetadata godoc
+// DidDocAllVersionMetadataEchoHandler godoc
 //
-//	@Summary		Resolve DID Document Version on did:cheqd
-//	@Description	Fetch specific all version of a DID Document ("DIDDoc") for a given DID and version ID
+//	@Summary		Resolve DID Document Versions on did:cheqd
+//	@Description	Fetch specific all versions of a DID Document ("DIDDoc") for a given DID
 //	@Tags			DID Resolution
 //	@Accept			application/did+ld+json,application/ld+json,application/did+json
 //	@Produce		application/did+ld+json,application/ld+json,application/did+json
-//	@Param			did			path		string	true	"Full DID with unique identifier"
-//	@Param			versionId	path		string	true	"version of a DID document"
-//	@Success		200			{object}	types.DidResolution
-//	@Failure		400			{object}	types.IdentityError
-//	@Failure		404			{object}	types.IdentityError
-//	@Failure		406			{object}	types.IdentityError
-//	@Failure		500			{object}	types.IdentityError
-//	@Router			/{did}/version/{versionId} [get]
+//	@Param			did	path		string	true	"Full DID with unique identifier"
+//	@Success		200	{object}	types.ResourceDereferencing{contentStream=types.DereferencedDidVersionsList}
+//	@Failure		400	{object}	types.IdentityError
+//	@Failure		404	{object}	types.IdentityError
+//	@Failure		406	{object}	types.IdentityError
+//	@Failure		500	{object}	types.IdentityError
+//	@Failure		501	{object}	types.IdentityError
+//	@Router			/{did}/versions [get]
 func DidDocAllVersionMetadataEchoHandler(c echo.Context) error {
 	return services.EchoWrapHandler(&DIDDocAllVersionMetadataRequestService{})(c)
 }
