a0fb445a1bd3a6dcebc675a325d5c2133cd7a486
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-03-17 12:50:17 +0500

    Update unit tests.

--- tests/unit-tests/did_doc_metadata_test.go
+++ tests/unit-tests/did_doc_metadata_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 var _ = Describe("Test NewResolutionDIDDocMetadata function", func() {
-	It("can resolution metadata with resource", func() {
+	It("can create the structure with resource", func() {
 		metadata := &didTypes.Metadata{
 			VersionId:   ValidVersionId,
 			Deactivated: false,
@@ -32,7 +32,7 @@ var _ = Describe("Test NewResolutionDIDDocMetadata function", func() {
 		Expect(result).To(Equal(expectedResult))
 	})
 
-	It("can resolution metadata without resource", func() {
+	It("can create the structure without resource", func() {
 		metadata := &didTypes.Metadata{
 			Created:     NotEmptyTimestamp,
 			Updated:     NotEmptyTimestamp,
