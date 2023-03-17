5a5e09ecaeede1b29b0f4b01894215a19d80cbc0
Andrew Nikitin <andrew.nikitin@cheqd.io>
2023-03-17 13:08:06 +0400

    Fix validation for query

--- services/diddoc/diddoc_query.go
+++ services/diddoc/diddoc_query.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/cheqd/did-resolver/services"
 	"github.com/cheqd/did-resolver/types"
+	"github.com/cheqd/did-resolver/utils"
 )
 
 type QueryDIDDocRequestService struct {
@@ -21,6 +22,18 @@ func (dd *QueryDIDDocRequestService) SpecificValidation(c services.ResolverConte
 	if err != nil {
 		return types.NewInvalidDIDUrlError(dd.Did, dd.RequestedContentType, err, dd.IsDereferencing)
 	}
+
+	// ToDo make list of supported queries
+	// For now we support only versionId
+	if dd.Version == "" {
+		return types.NewRepresentationNotSupportedError(dd.Did, dd.RequestedContentType, nil, dd.IsDereferencing)
+	}
+
+	// Validate that versionId is UUID
+	if !utils.IsValidUUID(dd.Version) {
+		return types.NewInvalidDIDUrlError(dd.Version, dd.RequestedContentType, nil, dd.IsDereferencing)
+	}
+	
 	return nil
 }
 
