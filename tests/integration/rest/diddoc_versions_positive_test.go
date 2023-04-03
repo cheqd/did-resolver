4f6d1e99d8b3aca882cb571aa680149a8b663216
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-04-03 13:21:36 +0500

    Add integration tests for testing get DIDDoc versions API.

---
+++ tests/integration/rest/diddoc_versions_positive_test.go
@@ -0,0 +1,46 @@
+//go:build integration
+
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	testconstants "github.com/cheqd/did-resolver/tests/constants"
+	"github.com/go-resty/resty/v2"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = DescribeTable("Positive: Get DIDDoc versions", func(testCase positiveTestCase) {
+	client := resty.New()
+
+	resp, err := client.R().
+		SetHeader("Accept", testCase.resolutionType).
+		Get(testCase.didURL)
+	Expect(err).To(BeNil())
+
+	var receivedDidDereferencing dereferencingResult
+	Expect(json.Unmarshal(resp.Body(), &receivedDidDereferencing)).To(BeNil())
+	Expect(testCase.expectedStatusCode).To(Equal(resp.StatusCode()))
+
+	var expectedDidDereferencing dereferencingResult
+	Expect(convertJsonFileToType(testCase.expectedJSONPath, &expectedDidDereferencing)).To(BeNil())
+
+	assertDidDereferencing(expectedDidDereferencing, receivedDidDereferencing)
+},
+
+	Entry(
+		"can get DIDDoc versions with an existent DID",
+		positiveTestCase{
+			didURL: fmt.Sprintf(
+				"http://localhost:8080/1.0/identifiers/%s/versions",
+				testconstants.IndyStyleTestnetDid,
+			),
+			resolutionType:     testconstants.DefaultResolutionType,
+			expectedJSONPath:   "testdata/diddoc_versions/diddoc_versions.json",
+			expectedStatusCode: http.StatusOK,
+		},
+	),
+)
