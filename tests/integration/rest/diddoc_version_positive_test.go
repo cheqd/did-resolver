a47c0605b0938afb4817a376d9941ab726d23e19
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-04-03 13:36:11 +0500

    Add integration tests for testing get DIDDoc version API.

---
+++ tests/integration/rest/diddoc_version_positive_test.go
@@ -0,0 +1,89 @@
+//go:build integration
+
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	testconstants "github.com/cheqd/did-resolver/tests/constants"
+	"github.com/go-resty/resty/v2"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = DescribeTable("Positive: Get DIDDoc version", func(testCase positiveTestCase) {
+	client := resty.New()
+
+	resp, err := client.R().
+		SetHeader("Accept", testCase.resolutionType).
+		Get(testCase.didURL)
+	Expect(err).To(BeNil())
+
+	var receivedDidDereferencing dereferencingResult
+	Expect(json.Unmarshal(resp.Body(), &receivedDidDereferencing)).To(BeNil())
+	Expect(testCase.expectedStatusCode).To(Equal(resp.StatusCode()))
+
+	var expectedDidDereferencing dereferencingResult
+	Expect(convertJsonFileToType(testCase.expectedJSONPath, &expectedDidDereferencing)).To(BeNil())
+
+	assertDidDereferencing(expectedDidDereferencing, receivedDidDereferencing)
+},
+
+	Entry(
+		"can get DIDDoc with an existent 22 bytes INDY style mainnet DID and versionId",
+		positiveTestCase{
+			didURL: fmt.Sprintf(
+				"http://localhost:8080/1.0/identifiers/%s/version/%s",
+				testconstants.IndyStyleMainnetDid,
+				"4fa8e367-c70e-533e-babf-3732d9761061",
+			),
+			resolutionType:     testconstants.DefaultResolutionType,
+			expectedJSONPath:   "testdata/diddoc_version/diddoc_version_indy_mainnet_did.json",
+			expectedStatusCode: http.StatusOK,
+		},
+	),
+
+	Entry(
+		"can get DIDDoc with an existent 22 bytes INDY style testnet DID and versionId",
+		positiveTestCase{
+			didURL: fmt.Sprintf(
+				"http://localhost:8080/1.0/identifiers/%s/version/%s",
+				testconstants.IndyStyleTestnetDid,
+				"60bb3b62-e0f0-545b-a552-63aab5cd1aef",
+			),
+			resolutionType:     testconstants.DefaultResolutionType,
+			expectedJSONPath:   "testdata/diddoc_version/diddoc_version_indy_testnet_did.json",
+			expectedStatusCode: http.StatusOK,
+		},
+	),
+
+	Entry(
+		"can get DIDDoc with an existent UUID style mainnet DID and versionId",
+		positiveTestCase{
+			didURL: fmt.Sprintf(
+				"http://localhost:8080/1.0/identifiers/%s/version/%s",
+				testconstants.UUIDStyleMainnetDid,
+				"76e546ee-78cd-5372-b34e-8b47461626e1",
+			),
+			resolutionType:     testconstants.DefaultResolutionType,
+			expectedJSONPath:   "testdata/diddoc_version/diddoc_version_uuid_mainnet_did.json",
+			expectedStatusCode: http.StatusOK,
+		},
+	),
+
+	Entry(
+		"can get DIDDoc with an existent UUID style testnet DID and versionId",
+		positiveTestCase{
+			didURL: fmt.Sprintf(
+				"http://localhost:8080/1.0/identifiers/%s/version/%s",
+				testconstants.UUIDStyleTestnetDid,
+				"e5615fc2-6f13-42b1-989c-49576a574cef",
+			),
+			resolutionType:     testconstants.DefaultResolutionType,
+			expectedJSONPath:   "testdata/diddoc_version/diddoc_version_uuid_testnet_did.json",
+			expectedStatusCode: http.StatusOK,
+		},
+	),
+)
