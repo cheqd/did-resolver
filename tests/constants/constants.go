84a9abe99443ee62e9221f80f1769949635a8e40
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-04-10 21:00:34 +0500

    Add constant variables for using integration tests instead of magic strings.

--- tests/constants/constants.go
+++ tests/constants/constants.go
@@ -23,6 +23,18 @@ var (
 )
 
 var (
+	IndyStyleMainnetDidIdentifier = "4fa8e367-c70e-533e-babf-3732d9761061"
+	IndyStyleTestnetDidIdentifier = "60bb3b62-e0f0-545b-a552-63aab5cd1aef"
+	UUIDStyleMainnetDidIdentifier = "76e546ee-78cd-5372-b34e-8b47461626e1"
+	UUIDStyleTestnetDidIdentifier = "e5615fc2-6f13-42b1-989c-49576a574cef"
+)
+
+var (
+	UUIDStyleTestnetDidResourceId                    = "9ba3922e-d5f5-4f53-b265-fc0d4e988c77"
+	OldIndy32CharStyleTestnetDidIdentifierResourceId = "214b8b61-a861-416b-a7e4-45533af40ada"
+)
+
+var (
 	NotExistentMainnetDid = fmt.Sprintf(DIDStructure, ValidMethod, ValidMainnetNamespace, NotExistentIdentifier)
 	NotExistentTestnetDid = fmt.Sprintf(DIDStructure, ValidMethod, ValidTestnetNamespace, NotExistentIdentifier)
 )
