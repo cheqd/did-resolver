e5abb6709707ee450493409e677952edcb6ecff6
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-06-18 15:45:21 +0500

    Add positive integration tests for testing resourceVersionTime query.

--- tests/integration/rest/resource/query/resource_version_time/positive_test.go
+++ tests/integration/rest/resource/query/resource_version_time/positive_test.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 
 	testconstants "github.com/cheqd/did-resolver/tests/constants"
 	utils "github.com/cheqd/did-resolver/tests/integration/rest"
@@ -33,20 +34,6 @@ var _ = DescribeTable("Positive: Get Collection of Resources with resourceVersio
 	utils.AssertDidDereferencing(expectedDidDereferencing, receivedDidDereferencing)
 },
 
-	Entry(
-		"can get collection of resources with an existent resourceVersionTime query parameter",
-		utils.PositiveTestCase{
-			DidURL: fmt.Sprintf(
-				"http://localhost:8080/1.0/identifiers/%s?resourceVersionTime=%s&resourceMetadata=true",
-				testconstants.UUIDStyleTestnetDid,
-				"2023-01-25T12:08:40Z",
-			),
-			ResolutionType:     testconstants.DefaultResolutionType,
-			ExpectedJSONPath:   "../../../testdata/query/resource_version_time/resource.json",
-			ExpectedStatusCode: http.StatusOK,
-		},
-	),
-
 	// TODO: add unit test for testing get resource with an old 16 characters INDY style DID
 	// and resourceVersionTime query parameter.
 
@@ -60,6 +47,62 @@ var _ = DescribeTable("Positive: Get Collection of Resources with resourceVersio
 			),
 			ResolutionType:     testconstants.DefaultResolutionType,
 			ExpectedJSONPath:   "../../../testdata/query/resource_version_time/resource_32_indy_did.json",
+			ExpectedStatusCode: http.StatusOK,
+		},
+	),
+
+	Entry(
+		"can get collection of resources with an existent resourceVersionTime query parameter (RFC3339 format)",
+		utils.PositiveTestCase{
+			DidURL: fmt.Sprintf(
+				"http://localhost:8080/1.0/identifiers/%s?resourceVersionTime=%s&resourceMetadata=true",
+				testconstants.UUIDStyleTestnetDid,
+				"2023-01-25T12:08:40Z",
+			),
+			ResolutionType:     testconstants.DefaultResolutionType,
+			ExpectedJSONPath:   "../../../testdata/query/resource_version_time/resource.json",
+			ExpectedStatusCode: http.StatusOK,
+		},
+	),
+
+	Entry(
+		"can get collection of resources with an existent resourceVersionTime query parameter (RFC3339Nano format)",
+		utils.PositiveTestCase{
+			DidURL: fmt.Sprintf(
+				"http://localhost:8080/1.0/identifiers/%s?resourceVersionTime=%s&resourceMetadata=true",
+				testconstants.UUIDStyleTestnetDid,
+				"2023-01-25T12:08:40.0Z",
+			),
+			ResolutionType:     testconstants.DefaultResolutionType,
+			ExpectedJSONPath:   "../../../testdata/query/resource_version_time/resource.json",
+			ExpectedStatusCode: http.StatusOK,
+		},
+	),
+
+	Entry(
+		"can get collection of resources with an existent resourceVersionTime query parameter (DateTime format)",
+		utils.PositiveTestCase{
+			DidURL: fmt.Sprintf(
+				"http://localhost:8080/1.0/identifiers/%s?resourceVersionTime=%s&resourceMetadata=true",
+				testconstants.UUIDStyleTestnetDid,
+				"2023-01-25"+url.PathEscape(testconstants.Space)+"12:08:40",
+			),
+			ResolutionType:     testconstants.DefaultResolutionType,
+			ExpectedJSONPath:   "../../../testdata/query/resource_version_time/resource.json",
+			ExpectedStatusCode: http.StatusOK,
+		},
+	),
+
+	Entry(
+		"can get collection of resources with an existent resourceVersionTime query parameter (DateOnly format)",
+		utils.PositiveTestCase{
+			DidURL: fmt.Sprintf(
+				"http://localhost:8080/1.0/identifiers/%s?resourceVersionTime=%s&resourceMetadata=true",
+				testconstants.UUIDStyleTestnetDid,
+				"2023-01-26",
+			),
+			ResolutionType:     testconstants.DefaultResolutionType,
+			ExpectedJSONPath:   "../../../testdata/query/resource_version_time/resource.json",
 			ExpectedStatusCode: http.StatusOK,
 		},
 	),
